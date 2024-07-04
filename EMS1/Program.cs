605a0c3d28f79583f188a44f1989a6130422e072
M-Ahmed-Iqbal <bcsf20m026@pucit.edu.pk>
2024-07-04 16:33:21 +0800

    commit

--- EMS1/Program.cs
+++ EMS1/Program.cs
@@ -4,3 +4,4 @@ EmployeeDAL dal = new EmployeeDAL();
 Employee employee = new Employee { Id = 1, Name = "usman" };
 dal.Save(employee);
 Console.WriteLine(dal.ReadEmployee().Id);
+Console.WriteLine("This is a change to my code");
